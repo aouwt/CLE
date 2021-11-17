a931d80c9d33042485816495726e917b6d5754f3
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-16 17:37:15 -0800

    Update cle.cpp

--- cle.cpp
+++ cle.cpp
@@ -20,8 +20,7 @@ class cell {
 		void runcell (void);
 		
 	private:
-		void distcolor (color color);
-		color filter (color fcolor, color bcolor);
+		color filter (color bcolor, color fcolor);
 		unsigned char gr (color color);
 		unsigned char gg (color color);
 		unsigned char gb (color color);
@@ -29,23 +28,54 @@ class cell {
 		color merge (struct beams colors);
 };
 
+color cell::filter (color bcolor, color fcolor) {
+	if (!bcolor) return;
+	unsigned char max = gr (fcolor);
+	if (gg (fcolor) > max) max = gg (fcolor);
+	if (gb (fcolor) > max) max = gb (fcolor);
+	if (!max) return;
+	
+	return rgb (
+		gr(bcolor) * (gr(fcolor) / max),
+		gg(bcolor) * (ggfcolor) / max),
+		gb(bcolor) * (gb(fcolor) / max)
+	);
+}
+
+color cell::merge (struct beams colors) {
+	unsigned int
+		r = gr(colors.l) + gr(colors.r) + gr(colors.u) + gr(colors.d),
+		g = gg(colors.l) + gg(colors.r) + gg(colors.u) + gg(colors.d),
+		b = gb(colors.l) + gb(colors.r) + gb(colors.u) + gb(colors.d);
+	if (r > 255) r = 255;
+	if (g > 255) g = 255;
+	if (b > 255) b = 255;
+	return rgb (r,g,b);
+}
+
+color cell:rgb (unsigned char r, unsigned char g, unsigned char b) { return (r << 16) | (g << 8) | b; }
+
+color cell:gr (color color) { return (color & 0xFF0000) >> 16; }
+color cell:gg (color color) { return (color & 0x00FF00) >> 8; }
+color cell:gb (color color) { return (color & 0x0000FF); }
+
 void cell::runcell (void) {
 	switch (op) {
-		case 'R': distcolor (RED); break;
-		case 'G': distcolor (GREEN); break;
-		case 'B': distcolor (BLUE); break;
-		case 'W': distcolor (WHITE); break;
-		case 'Y': distcolor (YELLOW); break;
-		case 'C': distcolor (CYAN); break;
-		case 'M': distcolor (MAGENTA); break;
+		case 'R': beam = {RED}; break;
+		case 'G': beam = {GREEN}; break;
+		case 'B': beam = {BLUE}; break;
+		case 'W': beam = {WHITE}; break;
+		case 'Y': beam = {YELLOW}; break;
+		case 'C': beam = {CYAN}; break;
+		case 'M': beam = {MAGENTA}; break;
 		
-		case 'r': distcolor (RED); op = '\0'; break;
-		case 'g': distcolor (GREEN); op = '\0'; break;
-		case 'b': distcolor (BLUE); op = '\0'; break;
-		case 'w': distcolor (WHITE); op = '\0'; break;
-		case 'y': distcolor (YELLOW); op = '\0'; break;
-		case 'c': distcolor (CYAN); op = '\0'; break;
-		case 'm': distcolor (MAGENTA); op = '\0'; break;
+		case 'r': beam = {RED}; op = '\0'; break;
+		case 'g': beam = {GREEN}; op = '\0'; break;
+		case 'b': beam = {BLUE}; op = '\0'; break;
+		case 'w': beam = {WHITE}; op = '\0'; break;
+		case 'y': beam = {YELLOW}; op = '\0'; break;
+		case 'c': beam = {CYAN}; op = '\0'; break;
+		case 'm': beam = {MAGENTA}; op = '\0'; break;
 		
 		case '?':
 			if (state)
@@ -116,7 +146,8 @@ void cell::runcell (void) {
 	}
 }
 
+
 void main () {
-	struct mfb_window *window = mfb_open ("CLEc", 800, 600);
+	//struct mfb_window *window = mfb_open ("CLEc", 800, 600);
 	
 }
