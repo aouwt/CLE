3e011b531beab067449af14b9ed5d59e3bfc11a6
aouwt <74026992+aouwt@users.noreply.github.com>
2021-11-22 19:30:20 -0500

    nvm ig that doesnt work

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-c++ $@ $(pkg-config --libs --cflags sdl2 SDL2_ttf) ./src/gui.cpp ./src/cle.cpp -o ./cle
+c++ $@ $(pkg-config --libs --cflags --static sdl2 SDL2_ttf) ./src/gui.cpp ./src/cle.cpp -o ./cle
